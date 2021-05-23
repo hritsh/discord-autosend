06f0b25d3cf0c3be3ea9d3b43b7205a6a0182f4f
Hritish Mahajan <65954042+hritsh@users.noreply.github.com>
2021-05-23 16:09:19 +0400

    Initial commit

---
+++ discord_spam.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from os import environ
+
+## Opening link and logging in
+def login(link,email,passwd):
+    ## Initialising/Installing Chromedriver
+    global driver
+    environ['WDM_LOCAL'] = '1'
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(link)
+    print("\nLogging in...")
+    driver.find_element_by_name('email').send_keys(email)
+    driver.find_element_by_name('password').send_keys(passwd)
+    driver.find_element_by_name('password').send_keys(Keys.RETURN)
+    sleep(5)
+    print("\nLogged in successfully")
+
+## Starting spam
+def spam(n,message):
+    for i in range(n):
+        actions = ActionChains(driver)
+        actions.send_keys(message)
+        actions.send_keys(Keys.ENTER)
+        actions.perform()
+        sleep(1)
+
+## Menu
+def main():
+    while True:
+        print("-"*100+"\n"+" "*36+"DISCORD SPAM / AUTO SEND"+"\n"+"-"*100+"\n")
+        email = input("Enter discord e-mail address: ")
+        passwd = input("Enter password: ")
+        link = input("Enter link to channel: ")
+        login(link,email,passwd)
+        message = input("\nEnter message to send: ")
+        n = int(input("Enter number of messages: "))
+        spam(n,message)
+        choice = input("\nDo you want to continue (y/n): ")
+        if (choice == "n"):
+            break
+
+main()
